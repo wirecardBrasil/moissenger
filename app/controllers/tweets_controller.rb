4de03e99b421700f2e36727820f3cc2ef58a53ab
Gabriela Emi Sanada <emisanada@hotmail.com>
2017-05-05 15:11:42 -0300

    Fix create tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -12,11 +12,11 @@ class TweetsController < ApplicationController
   end
 
   def create
-  	puts "============== #{params["tweet"].inspect}"
-  	@tweet = Tweet.new(params["tweet"])
-  	@tweet.save!
-  	render "index"
+    puts "============== #{params["tweet"].inspect}"
+    tweet = Tweet.new(tweet: params["tweet"]["tweet"])
+    tweet.save
+    render "index"
   rescue => e
-  	puts "-------------> #{e.backtrace}"
+    puts "-------------> #{e.message}"
   end
 end

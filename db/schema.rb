ab57a96e7b5aa566d36e01e5811ed1aca13ab659
Gabriela Emi Sanada <emisanada@hotmail.com>
2017-08-04 17:45:47 -0300

    Add Logout button

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170804134031) do
+ActiveRecord::Schema.define(version: 20170804203239) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.string   "tweet",                                                                 null: false
@@ -23,17 +31,18 @@ ActiveRecord::Schema.define(version: 20170804134031) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",              null: false
-    t.string   "user_name",          null: false
-    t.string   "name",               null: false
+    t.string   "email",               null: false
+    t.string   "user_name",           null: false
+    t.string   "name",                null: false
     t.string   "avatar"
-    t.string   "encrypted_password", null: false
-    t.string   "department",         null: false
-    t.string   "about",              null: false
+    t.string   "encrypted_password",  null: false
+    t.string   "department",          null: false
+    t.string   "about",               null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "provider"
     t.string   "uid"
+    t.datetime "remember_created_at"
   end
 
 end

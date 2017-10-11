3de5256fe68578ef73906ca6a734b62c61406be0
Gabriela Emi Sanada <emisanada@hotmail.com>
2017-10-11 15:26:04 -0300

    Improve login flash erro and success msg

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,23 +1,23 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
-	def google_oauth2
+  def google_oauth2
 
-		if User.domain_valid?(request.env["omniauth.auth"].info.email)
+    if User.domain_valid?(request.env["omniauth.auth"].info.email)
 
-		  @user = User.find_for_google_oauth2(request.env["omniauth.auth"])
+      @user = User.find_for_google_oauth2(request.env["omniauth.auth"])
 
-		  if @user.persisted?
-		    flash[:notice] = "Sucesso"
-		    sign_in_and_redirect @user, event: :authentication
-		  else
-		    flash[:error] = "Ops"
-		    redirect_to new_user_session_url
-		  end
+      if @user.persisted?
+        flash[:notice] = "Login realizado com sucesso!"
+        sign_in_and_redirect @user, event: :authentication
+      else
+        flash[:error] = "Ops! Houve um erro com o seu login."
+        redirect_to new_user_session_url
+      end
 
-		else
-			flash[:error] = "Ops de novo"
-			redirect_to new_user_session_url
-		end
-	end
+    else
+      flash[:error] = "Ops de novo!"
+      redirect_to new_user_session_url
+    end
+  end
 
 end
